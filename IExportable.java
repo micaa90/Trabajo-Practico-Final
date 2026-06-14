e16e940d53ee28c07ba0e9f3c15c8ebe6cad9004
Mica <mgimenez322@alumnos.iua.edu.ar>
2026-06-14 17:53:26 -0300

    subo clase de interfaz para ya tenerlo

---
+++ IExportable.java
@@ -0,0 +1,11 @@
+
+/**
+ * Interfaz que define el contrato formal para la exportación de datos.
+ * Cualquier clase que la implemente mostrará una representación textual 
+ * de su estado interno.
+ * @author Gimenez Micaela, Garcia Ayelen, Petrangli Dylan
+ * @version 1.0
+ */
+public interface IExportable {
+    public String generarLineaArchivo();
+}
